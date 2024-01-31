a77af125f788c97017d999a6934e0971cebdc9a7
nimuy99 <nimuy99@gmail.com>
2024-01-31 23:03:19 +0900

    [Feat] 아이템 클릭 시 버튼 텍스트 변경 및 코드 주석 처리

--- app/src/main/java/com/umc/android/packit/OrderCouponRVAdapter.kt
+++ app/src/main/java/com/umc/android/packit/OrderCouponRVAdapter.kt
@@ -1,32 +1,48 @@
 package com.umc.android.packit
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.umc.android.packit.databinding.ItemOrderCouponBinding
 
 class OrderCouponRVAdapter(private val couponList: List<OrderCoupon>)
     :RecyclerView.Adapter<OrderCouponRVAdapter.ViewHolder>() {
+    interface ItemClick {  //클릭이벤트추가부분
+        fun onClick(position : Int)
+    }
 
-        // 쿠폰 데이터 개수
-        override fun getItemCount(): Int = couponList.size
+//    private lateinit var itemClick : ItemClick  //클릭이벤트추가부분
+     var itemClick: ItemClick? = null // nullable로 선언
 
-        // 뷰홀더 생성 (아이템 뷰 생성)
-        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): OrderCouponRVAdapter.ViewHolder {
-            val binding: ItemOrderCouponBinding = ItemOrderCouponBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+    // 쿠폰 데이터 개수
 
-            return ViewHolder(binding)
-        }
-
-        // 뷰홀더 쿠폰 데이터 바인딩
-        override fun onBindViewHolder(holder: OrderCouponRVAdapter.ViewHolder, position: Int) {
-            holder.bind(couponList[position])
-        }
+    override fun getItemCount(): Int = couponList.size
 
 
-        inner class ViewHolder(val binding: ItemOrderCouponBinding) : RecyclerView.ViewHolder(binding.root) {
-            fun bind(coupon: OrderCoupon){
-                binding.itemCouponNameTv.text = coupon.name
+    // 뷰홀더 생성
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): OrderCouponRVAdapter.ViewHolder {
+        val binding: ItemOrderCouponBinding = ItemOrderCouponBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
+    }
+
+    // 뷰홀더 쿠폰 데이터 바인딩
+    override fun onBindViewHolder(holder: OrderCouponRVAdapter.ViewHolder, position: Int) {
+        holder.bind(couponList[position])
+        holder.binding.itemCoupon.setOnClickListener {
+            itemClick?.let {
+                it.onClick(position)
             }
         }
+    }
+
+
+    inner class ViewHolder(val binding: ItemOrderCouponBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(coupon: OrderCoupon){
+            binding.itemCouponNameTv.text = coupon.name
+
+
+        }
+    }
 }
--- app/src/main/java/com/umc/android/packit/OrderFragment.kt
+++ app/src/main/java/com/umc/android/packit/OrderFragment.kt
@@ -10,48 +10,48 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.android.packit.databinding.FragmentOrderBinding
 
 
-class OrderFragment : AppCompatActivity() {
+class OrderFragment() : AppCompatActivity() {
 
     // 변수 선언
     private lateinit var binding: FragmentOrderBinding
-    private var isClicked = false // 쿠폰 버튼
-    private var isChecked = false // 체크 버튼
     private lateinit var couponBackground: CardView // 쿠폰 배경
+    private var isClicked = false // 쿠폰 버튼 상태
+    private var isChecked = false // 체크 버튼 상태
 
-
+    private val couponList = listOf(
+        OrderCoupon("[첫 주문] 5% 할인"),
+        OrderCoupon("[누적 주문 10회] 5% 할인"),
+        OrderCoupon("[누적 주문 10회] 15% 할인"),
+        OrderCoupon("[누적 주문 10회] 20% 할인"),
+        OrderCoupon("쿠폰 미사용")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // xml 바인딩 작업
         binding = FragmentOrderBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 더미 데이터
-        val profileList = ArrayList<OrderCoupon>()
-        profileList.add(OrderCoupon("[첫 주문] 5% 할인"))
-        profileList.add(OrderCoupon("[누적 주문 10회] 5% 할인"))
-        profileList.add(OrderCoupon("[누적 주문 10회] 15% 할인"))
-        profileList.add(OrderCoupon("[누적 주문 10회] 20% 할인"))
-        profileList.add(OrderCoupon("쿠폰 미사용"))
+        init()  // 화면 초기화
 
-
-        binding.orderCouponRecyclerView.adapter = OrderCouponRVAdapter(profileList)
+        // 리사이클러뷰 연결
+        val adapter = OrderCouponRVAdapter(couponList)
+        binding.orderCouponRecyclerView.adapter = adapter
         binding.orderCouponRecyclerView.layoutManager= LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
+        // 아이템 클릭 이벤트 설정
+        adapter.itemClick  = object : OrderCouponRVAdapter.ItemClick {  //클릭이벤트추가부분
+            override fun onClick(position: Int) {
+                // 클릭한 쿠폰으로 텍스트 변경
+                binding.orderCouponSelectedBtn.text = couponList[position].name
+                isClicked=false  // 클릭 버튼 상태 바꿔 주면서 쿠폰 리스트 숨기기
+                hideCouponList()
+            }
+        }
 
-        // couponBackground 초기화 -> init으로 묶을것
-        couponBackground = binding.orderCouponBackgroundView
-
-
-        couponBackground.visibility = View.INVISIBLE
-
-        binding.orderCheckOffBtnIv.visibility = View.VISIBLE
-        binding.orderCheckOnBtnIv.visibility = View.GONE
-
-
+        // 쿠폰 선택 버튼
         binding.orderCouponSelectedBtn.setOnClickListener {
-            showCouponList()
+            clickCouponSelection()
         }
 
         // 하단 체크 버튼
@@ -63,24 +63,40 @@ class OrderFragment : AppCompatActivity() {
         }
     }
 
-    private fun showCouponList() {
+    // 화면 초기화 함수
+    private fun init() {
+        // 쿠폰 리스트 숨김 상태
+        couponBackground = binding.orderCouponBackgroundView
+        couponBackground.visibility = View.INVISIBLE
+
+        // 체크 버튼 해제 상태
+        binding.orderCheckOffBtnIv.visibility = View.VISIBLE
+        binding.orderCheckOnBtnIv.visibility = View.GONE
+    }
+
+    // 쿠폰 클릭 시, 쿠폰 리스트 띄우기
+    private fun clickCouponSelection() {
         isClicked = !isClicked // 상태 전환
+        if (isClicked) showCouponList() else hideCouponList()
+    }
+
+    // 쿠폰 리스트 보이기
+    private fun showCouponList() {
+        couponBackground.visibility = View.VISIBLE
+        // 아래로 슬라이드
         val slideDown = ObjectAnimator.ofFloat(couponBackground, "translationY", 0f, 55f)
+        slideDown.duration = 300
+        slideDown.start()
+    }
+
+    // 쿠폰 리스트 숨기기
+    private fun hideCouponList() {
+        // 위로 슬라이드
         val slideUp = ObjectAnimator.ofFloat(couponBackground, "translationY", 55f, 0f)
-
-        if (isClicked) {
-                couponBackground.visibility = View.VISIBLE
-
-            slideDown.duration = 300
-            slideDown.start()
-        } else {
-            // 위로 슬라이드
-            slideUp.duration = 300
-            slideUp.start()
-//            // 체크되지 않은 상태로 변경
-            slideUp.doOnEnd {
-                couponBackground.visibility = View.GONE
-            }
+        slideUp.duration = 300
+        slideUp.start()
+        slideUp.doOnEnd {
+            couponBackground.visibility = View.GONE
         }
     }
 
@@ -88,12 +104,10 @@ class OrderFragment : AppCompatActivity() {
     private fun checkButtonState() {
         isChecked = !isChecked // 상태 전환
 
-        if (isChecked) {
-            // 체크된 상태로 변경
+        if (isChecked) {    // 체크된 상태로 변경
             binding.orderCheckOffBtnIv.visibility = View.GONE
             binding.orderCheckOnBtnIv.visibility = View.VISIBLE
-        } else {
-            // 체크되지 않은 상태로 변경
+        } else {            // 체크되지 않은 상태로 변경
             binding.orderCheckOffBtnIv.visibility = View.VISIBLE
             binding.orderCheckOnBtnIv.visibility = View.GONE
         }
